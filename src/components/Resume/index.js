b4ab3e1fa5146063faae317dd6a18e4663efba5c
jcomp-03@James-Desktop <jcomp_03@yahoo.com>
2022-05-22 16:58:57 -0400

    Finished with Resume component and its styling.

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -1,11 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
+// import validator function for email
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFileArrowDown } from "@fortawesome/free-solid-svg-icons";
 
 function Resume() {
   return (
-    <div className="border border-success border-2 mh-100">
-      <div>Text which belongs to Resume section</div>
-      <div>More text</div>
-    </div>
+    <section id="section-resume" className="row">
+      <h1 className="d-flex justify-content-center align-items-center">
+        Click on that neat FontAwesome icon to find my resume!
+      </h1>
+      <a
+        href="https://docs.google.com/document/d/1A2oUGUtUcXnG62woIMNxKq_nc_lNltowWDCQ9uOm0ls/edit?usp=sharing"
+        target="_blank"
+        rel="noreferrer"
+        className="text-center"
+      >
+        <FontAwesomeIcon
+          icon={faFileArrowDown}
+          className="font-awesome-code-icon"
+        ></FontAwesomeIcon>
+      </a>
+    </section>
   );
 }
 
